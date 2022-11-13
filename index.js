81105bbb1445fe2818e450d6487e808bfbc83bdd
badelgeek <badelgeek@gmail.com>
2022-11-13 10:13:29 +0100

    Improve

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
-document.onkeyup = handleKeyUp;
+// Declarations
+const adEl = document.querySelector('.ad');
+const h1El = document.querySelector('h1');
 const commandEl = document.querySelector(".command");
+const instructionEl = document.querySelector('.instruction');
+const cpcScreenEl = document.querySelector('.cpc-screen');
+const playEl = document.querySelector('.play');
 
+// Functions
 function handleKeyUp(event) {
    if (event.key !== "Enter") {
       return commandEl.textContent += event.key;
    }
 
    if (commandEl.textContent === 'codepen') {
-      document.querySelector('.ad').classList.add('back80');
-      document.querySelector('h1').classList.add('back80');
-      document.querySelector('.instruction').textContent = 'Yeah !!!';
+      adEl.classList.add('back80');
+      h1El.classList.add('back80');
+      instructionEl.textContent = 'Yeah !!!';
+      playEl.style.display = 'grid';
       return commandEl.textContent = "Welcome Back to the 1980's !!!";
    }
 
-   document.querySelector('.cpc-screen').classList.add('error');
-   document.querySelector('.ad').classList.add('error');
-   document.querySelector('.instruction').textContent = 'ERROR !!!';
+   cpcScreenEl.classList.add('error');
+   adEl.classList.add('error');
+   instructionEl.textContent = 'ERROR !!!';
+
    setTimeout(() => {
-      document.querySelector('.cpc-screen').classList.remove('error');
-      document.querySelector('.ad').classList.remove('error');
-      document.querySelector('.instruction').textContent = 'Try again !!!';
+      cpcScreenEl.classList.remove('error');
+      adEl.classList.remove('error');
+      instructionEl.textContent = 'Try again !!!';
       commandEl.textContent = ""
    }, 500);
    return (commandEl.textContent = "ERROR");
-
 }
+
+// Event Handler
+document.onkeyup = handleKeyUp;
